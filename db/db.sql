fb4a69bfad287c443786e15d695989666dfba35d
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-06-03 15:23:15 -0500

    endpoint crear usuario

---
+++ db/db.sql
@@ -0,0 +1,13 @@
+USE delivery;
+
+CREATE TABLE users(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(180) NOT NULL UNIQUE,
+    name VARCHAR(90) NOT NULL,
+    lastname VARCHAR(90) NOT NULL,
+    phone VARCHAR(90) NOT NULL UNIQUE,
+    image VARCHAR(255) NULL,
+    password VARCHAR(90) NOT NULL,
+    created_at timestamp default current_timestamp not null,
+    updated_at timestamp default current_timestamp not null
+);
