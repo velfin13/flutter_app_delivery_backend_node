513489e4c1f7dd0269db0e1185786950ad3ae045
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-06-06 12:54:53 -0500

    cinfiguranda las variables de entorno

--- server.js
+++ server.js
@@ -1,56 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const logger = require('morgan');
-const cors = require('cors');
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
 /*
-* IMPORTAR RUTAS
-*/
-const usersRoutes = require('./routes/userRoutes');
+ * IMPORTAR RUTAS
+ */
+const usersRoutes = require("./routes/userRoutes");
 
 const port = process.env.PORT || 3000;
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
-
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(cors());
 
-app.disable('x-powered-by');
-
-app.set('port', port);
+app.disable("x-powered-by");
 
 /*
-* LLAMADO DE LAS RUTAS
-*/
+ * LLAMADO DE LAS RUTAS
+ */
 usersRoutes(app);
 
-
-server.listen(3000,'localhost', function() {
-    console.log('Aplicacion de NodeJS ' + port + ' Iniciada...')
+server.listen(port, "localhost", function () {
+  console.log(`http://localhost:${port}`);
 });
-
 
 // ERROR HANDLER
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(err.status || 500).send(err.stack);
+  console.log(err);
+  res.status(err.status || 500).send(err.stack);
 });
 
-app.get('/',  (req, res) => {
-    res.send('Ruta raiz del backend');
+app.get("/", (req, res) => {
+  res.send("Ruta raiz del backend");
 });
-
 
 module.exports = {
-    app: app,
-    server: server
-}
+  app: app,
+  server: server,
+};
 
 // 200 - ES UN RESPUESTA EXITOSA
 // 404 - SIGNIFICA QUE LA URL NO EXISTE
