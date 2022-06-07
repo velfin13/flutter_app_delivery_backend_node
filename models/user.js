fdeacd02aa7bdc941abf78406a6c1ecb1ca35eeb
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-06-06 19:06:32 -0500

    encriptando contraseña

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
-const db = require('../config/config');
+const db = require("../config/config");
+const bcrypt = require("bcryptjs");
 
 const User = {};
 
-User.create = (user, result) => {
-
-    const sql = `
+User.create = async (user, result) => {
+  const hash = await bcrypt.hash(user.password, 10);
+  const sql = `
         INSERT INTO
             users(
                 email,
@@ -19,31 +20,28 @@ User.create = (user, result) => {
         VALUES(?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
-    db.query
-    (
-        sql,
-        [
-            user.email,
-            user.name,
-            user.lastname,
-            user.phone,
-            user.image,
-            user.password,
-            new Date(),
-            new Date()
-        ],
-        (err, res) => {
-            if (err) {
-                console.log('Error:', err);
-                result(err, null);
-            }
-            else {
-                console.log('Id del nuevo usuario:', res.insertId);
-                result(null, res.insertId);
-            }
-        }
-    )
-
-}
+  db.query(
+    sql,
+    [
+      user.email,
+      user.name,
+      user.lastname,
+      user.phone,
+      user.image,
+      hash,
+      new Date(),
+      new Date(),
+    ],
+    (err, res) => {
+      if (err) {
+        console.log("Error:", err);
+        result(err, null);
+      } else {
+        console.log("Id del nuevo usuario:", res.insertId);
+        result(null, res.insertId);
+      }
+    }
+  );
+};
 
 module.exports = User;
